b6823914c4775f04777d255951d7579975bb6fa9
urock-namhyunwooA <hyunwoo.nam@urock.kr>
2025-12-01 02:00:29 +0000

    이전 기능 추가 롤백, vercel serverless 환경에 fs이 없음 포스트 두개 작성

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
-import '../styles/globals.css'
-import { AdminProvider } from "../contexts/admin-contexts";
-import AdminModeHandler from "../components/admin-mode-handler";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <AdminProvider>
-      <AdminModeHandler />
       <Component {...pageProps} />
-    </AdminProvider>
   );
 }
