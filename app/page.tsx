575748b25fc555c1978c83edd3d4649032952281
urock-namhyunwooA <hyunwoo.nam@urock.kr>
2025-12-11 06:23:31 +0000

    포스트 목록 캐싱 비활성화

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ export async function generateMetadata(): Promise<Metadata> {
   return generateBaseMetadata(settings);
 }
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const allPostsData = await getSortedPostsData();
   const settings = await getSettings();
